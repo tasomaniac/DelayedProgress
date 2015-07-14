a39e1e00c2e097352935b1fc73d15f0e0c5cc200
Said Tahsin Dane <tasomaniac@gmail.com>
2015-07-14 17:57:37 +0300

    refactor: include AppCompat Toolbar

--- sample/src/main/java/com/tasomaniac/android/widget/example/MainActivity.java
+++ sample/src/main/java/com/tasomaniac/android/widget/example/MainActivity.java
@@ -1,14 +1,15 @@
 package com.tasomaniac.android.widget.example;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.ListActivity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.ListFragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.util.Pair;
@@ -26,58 +27,16 @@ import android.widget.TwoLineListItem;
 import com.tasomaniac.android.widget.ContentLoadingProgressDialog;
 
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setListAdapter(new MyAdapter(this, new Pair[]{
-                Pair.create("Regular ProgressDialog", ""),
-                Pair.create("Delayed ProgressDialog", "ProgressDialog that waits a minimum time to be dismissed before showing"),
-                Pair.create("With custom delay", "Basic progress dialog with default 2 seconds delay. (Default it 500ms)"),
-                Pair.create("Min Show Time", "Once visible, the progress bar will be visible for a minimum amount of time to avoid flashes in the UI.\nWhen you click this, the ProgressDialog will be delayed 1 seconds and then dismissed after exactly 100ms. Because of the minShowTime, it will be visible for 500ms."),
-                Pair.create("No Min Show Time", "This does not have minShowTime (and causes flashes in the UI) to see the difference.")
-        }));
-    }
 
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
 
-        switch (position) {
-            case 0:
-                ProgressDialog.show(this, null, "Loading", true, true);
-                break;
-            case 1:
-                ContentLoadingProgressDialog.showDelayed(this, null, "Loading", true, true);
-                break;
-            case 2:
-                ContentLoadingProgressDialog.makeDelayed(this, null, "Loading", true, true).minDelay(2000).show();
-                break;
-            case 3: {
-                final ContentLoadingProgressDialog dialog = ContentLoadingProgressDialog.makeDelayed(this, null, "Loading", true, true).minDelay(1000);
-                dialog.show();
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        dialog.dismiss();
-                        Toast.makeText(getApplicationContext(), "Items are loaded", Toast.LENGTH_SHORT).show();
-                    }
-                }, 1100);
-                break;
-            }
-            case 4: {
-                final ContentLoadingProgressDialog dialog = ContentLoadingProgressDialog.makeDelayed(this, null, "Loading", true, true).minDelay(1000).minShowTime(0);
-                dialog.show();
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        dialog.dismiss();
-                        Toast.makeText(getApplicationContext(), "Items are loaded", Toast.LENGTH_SHORT).show();
-                    }
-                }, 1100);
-                break;
-            }
+        if (savedInstanceState == null) {
+            MainListFragment fragment = new MainListFragment();
+            getSupportFragmentManager().beginTransaction().replace(android.R.id.content, fragment).commit();
         }
     }
 
@@ -106,7 +65,7 @@ public class MainActivity extends ListActivity {
         }
     }
 
-    public class MyAdapter extends ArrayAdapter<Pair<String, String>> {
+    public static class MyAdapter extends ArrayAdapter<Pair<String, String>> {
         public MyAdapter(Context context, Pair<String, String>[] list) {
             super(context, android.R.layout.simple_list_item_2, list);
         }
@@ -116,7 +75,7 @@ public class MainActivity extends ListActivity {
             TwoLineListItem twoLineListItem;
 
             if (convertView == null) {
-                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 twoLineListItem = (TwoLineListItem) inflater.inflate(android.R.layout.simple_list_item_2, null);
             } else {
                 twoLineListItem = (TwoLineListItem) convertView;
@@ -126,6 +85,60 @@ public class MainActivity extends ListActivity {
             twoLineListItem.getText2().setText(getItem(position).second);
 
             return twoLineListItem;
+        }
+    }
+
+    public static class MainListFragment extends ListFragment {
+
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            setListAdapter(new MyAdapter(getActivity(), new Pair[]{
+                    Pair.create("Regular ProgressDialog", ""),
+                    Pair.create("Delayed ProgressDialog", "ProgressDialog that waits a minimum time to be dismissed before showing"),
+                    Pair.create("With custom delay", "Basic progress dialog with default 2 seconds delay. (Default it 500ms)"),
+                    Pair.create("Min Show Time", "Once visible, the progress bar will be visible for a minimum amount of time to avoid flashes in the UI.\nWhen you click this, the ProgressDialog will be delayed 1 seconds and then dismissed after exactly 100ms. Because of the minShowTime, it will be visible for 500ms."),
+                    Pair.create("No Min Show Time", "This does not have minShowTime (and causes flashes in the UI) to see the difference.")
+            }));
+        }
+
+        public void onListItemClick(ListView listView, View view, int position, long id) {
+
+            switch (position) {
+                case 0:
+                    ProgressDialog.show(getActivity(), null, "Loading", true, true);
+                    break;
+                case 1:
+                    ContentLoadingProgressDialog.showDelayed(getActivity(), null, "Loading", true, true);
+                    break;
+                case 2:
+                    ContentLoadingProgressDialog.makeDelayed(getActivity(), null, "Loading", true, true).minDelay(2000).show();
+                    break;
+                case 3: {
+                    final ContentLoadingProgressDialog dialog = ContentLoadingProgressDialog.makeDelayed(getActivity(), null, "Loading", true, true).minDelay(1000);
+                    dialog.show();
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            dialog.dismiss();
+                            Toast.makeText(getActivity(), "Items are loaded", Toast.LENGTH_SHORT).show();
+                        }
+                    }, 1100);
+                    break;
+                }
+                case 4: {
+                    final ContentLoadingProgressDialog dialog = ContentLoadingProgressDialog.makeDelayed(getActivity(), null, "Loading", true, true).minDelay(1000).minShowTime(0);
+                    dialog.show();
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            dialog.dismiss();
+                            Toast.makeText(getActivity(), "Items are loaded", Toast.LENGTH_SHORT).show();
+                        }
+                    }, 1100);
+                    break;
+                }
+            }
         }
     }
 }
